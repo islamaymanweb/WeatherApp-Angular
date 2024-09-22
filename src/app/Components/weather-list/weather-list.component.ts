e9b50fe5526addc63371d4f6a394e83227e06e6b
islamayman <islamwebdevelopment>
2024-09-22 16:04:30 +0300

    First Upload

---
+++ src/app/Components/weather-list/weather-list.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { BackGroundColorDirective } from '../../Directives/back-ground-color.directive';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-weather-list',
+  standalone: true,
+  imports: [BackGroundColorDirective, FormsModule],
+  templateUrl: './weather-list.component.html',
+  styleUrl: './weather-list.component.css'
+})
+export class WeatherListComponent {
+  searchText: string = '';
+  filteredWeatherList: any[];
+  sortBy: string = '';
+  sortAscending: boolean = false;
+  weatherList: any[] = [
+    { city: 'New York', temperature: 20 },
+    { city: 'Cairo', temperature: 30 },
+    { city: 'Tokyo', temperature: 9 },
+  ];
+  constructor(private router: Router) {
+    this.filteredWeatherList = this.weatherList;
+  }
+  navigateToWeather(temperature: number) {
+    this.router.navigate(['/weather'], { queryParams: { temperature } });
+  }
+  filterWeatherList() {
+    if (!this.searchText) {
+      this.filteredWeatherList = this.weatherList;
+    } else {
+      this.filteredWeatherList = this.weatherList.filter((cityWeather) => {
+        return cityWeather.city
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase());
+      });
+    }
+  }
+  sortWeatherList(property: string) {
+    this.sortBy = property;
+    this.sortAscending = !this.sortAscending;
+    this.filteredWeatherList.sort((a, b) => {
+      const avalue = a[property];
+      const bvalue = b[property];
+      if (avalue < bvalue) {
+        return this.sortAscending ? -1 : 1;
+      }
+      else if (avalue > bvalue) {
+        return this.sortAscending ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+}
